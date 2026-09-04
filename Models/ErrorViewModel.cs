d74b96ac340a0de5af1e145073b881b981278a15
Marven Mathelier <marven250@hotmail.com>
2026-09-04 11:49:46 -0400

    Unit 3 Assessment Starter Code

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Assessment4.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Models/Melon.cs
@@ -0,0 +1,16 @@
+﻿namespace Assessment4.Models
+{
+    public class Melon
+    {
+        public static List<Melon> mostLovedMelons = new List<Melon> {
+            new Melon { Img = "images/crenshaw.png", Name = "Crenshaw", NumLoves = 584 },
+            new Melon { Img = "images/jubilee_watermelon.jpg", Name = "Jubilee Watermelon", NumLoves = 601 },
+            new Melon { Img = "images/sugar_baby_watermelon.gif", Name = "Sugar Baby Watermelon", NumLoves = 587 },
+            new Melon { Img = "images/texas_garden_watermelon.jpeg", Name = "Texas Golden Watermelon", NumLoves = 598 }
+        };
+
+        public string? Img { get; set; }
+        public string? Name { get; set; }  
+        public int NumLoves { get; set; }
+    }
+}
