f9a49a137f0b89c69eee67bf5e6ea09fc834aba2
Zach Sandifer <jmfwljn@jmfamily.com>
2026-09-04 15:18:36 -0400

    Complete Assessment 3

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,6 +15,10 @@ namespace Assessment4.Controllers
 
         public IActionResult Index()
         {
+            if (!string.IsNullOrWhiteSpace(HttpContext.Session.GetString("name")))
+            {
+                return RedirectToAction(nameof(TopMelons));
+            }
             return View();
         }
 
@@ -27,6 +31,47 @@ namespace Assessment4.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpGet("top-melons")]
+        public IActionResult TopMelons()
+        {
+            if (string.IsNullOrWhiteSpace(HttpContext.Session.GetString("name")))
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            ViewData["Melons"] = Melon.mostLovedMelons;
+
+            return View("TopMelons");
+        }
+
+        [HttpPost("get-name")]
+        public IActionResult GetName(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                return BadRequest("You entered an empty value as your name. Go back and enter your real name.");
+            }
+
+            HttpContext.Session.SetString("name", name);
+            _logger.LogInformation("Session name set to {Name}", name);
+
+            return RedirectToAction(nameof(TopMelons));
+        }
+
+        [HttpPost("love-melon")]
+        public IActionResult LoveMelon(string melonName)
+        {
+            var lovedMelon = Melon.mostLovedMelons.FirstOrDefault(m => m.Name == melonName);
+
+            if (lovedMelon != null)
+            {
+                lovedMelon.NumLoves++;
+                _logger.LogInformation("{MelonName} loved. New total: {NumLoves}", lovedMelon.Name, lovedMelon.NumLoves);
+            }
+
+            return View("ThankYou");
         }
     }
 }
